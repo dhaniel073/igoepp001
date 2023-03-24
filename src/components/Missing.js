c8cc659c3ae4734aa4159ec646a451d1c0159d51
emmanuel daniel <emmadan073@gmail.com>
2023-03-24 21:46:49 +0100

    adding another paragraph to 404 page

--- src/components/Missing.js
+++ src/components/Missing.js
@@ -5,6 +5,7 @@ const Missing = () => {
         <article style={{ padding: "100px" }}>
             <h1>Oops!</h1>
             <p>Page Not Found</p>
+            <p>Seems you have a wrong url on your browser</p>
             <div className="flexGrow">
                 <Link to="/">Visit Our Homepage</Link>
             </div>
